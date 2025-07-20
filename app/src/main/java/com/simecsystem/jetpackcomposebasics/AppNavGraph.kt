362278e2205d122aa8b6e7118a304d71bf0c02d6
Md Shakhawat Hossain Shahin <shakhawat.dev@gmail.com>
2025-07-20 18:19:03 +0600

    Initial commit

---
+++ app/src/main/java/com/simecsystem/jetpackcomposebasics/AppNavGraph.kt
@@ -0,0 +1,11 @@
+package com.simecsystem.jetpackcomposebasics
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+
+}
